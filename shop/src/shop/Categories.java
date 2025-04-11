a284c18b6d404cff0353214024eb6d888d1c86f6
Miguel Diaz <miguelitodiaz169@gmail.com>
2025-04-11 13:47:13 -0500

    create new classes 🌤️

---
+++ shop/src/shop/Categories.java
@@ -0,0 +1,12 @@
+package shop;
+
+public class Categories {
+    private int pk;
+    private String name;
+    private String productOrService;
+    
+    public Categories(int pk, String name, String productOrService) {
+        this.pk = pk;
+    }
+    
+}

6063c690295a25228bb322ec0f984b73364679a2
Miguel Diaz <miguelitodiaz169@gmail.com>
2025-04-11 14:11:44 -0500

    add new classes ✨

--- shop/src/shop/Services.java
+++ shop/src/shop/Services.java
@@ -1,5 +1,36 @@
 package shop;
 
 public class Services {
+    // attributes
+    private int pk;
+    private int price;
+    private String name;
+    private Boolean active;
+    private int amount;
+    
+    // composition root
+    private Services[] services;
+
+
+    // methods
+    public void addService(){
+
+    }
+
+    public void deleteService(){
+
+    }
+
+    public void editService(){
+
+    }
+
+    public String[] viewService(){
+        String string[] = new String[5];
+        return string ;
+    }
+
+    
+
     
 }

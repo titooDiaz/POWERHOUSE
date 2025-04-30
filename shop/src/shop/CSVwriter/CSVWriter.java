99a0ac7696f17cfb98229c3930501d25919898f2
Miguel Angel <miguelitodiaz169@gmail.com>
2025-04-30 18:28:05 -0500

    save CSV files 💾

---
+++ shop/src/shop/CSVwriter/CSVWriter.java
@@ -0,0 +1,45 @@
+package shop.CSVwriter;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+// Import user information class
+import shop.User;
+
+public class CSVWriter {
+
+    private static int currentPk = 1; // You can load this value from the file if you want continuity
+
+    public static void appendUserToCSV(User user) {
+        String filePath = "src/resources/data/Users/user.csv";
+        boolean fileExists = new java.io.File(filePath).exists();
+
+        try (FileWriter fw = new FileWriter(filePath, true);
+             PrintWriter pw = new PrintWriter(fw)) {
+
+            // Write header only if the file does not exist
+            if (!fileExists) {
+                pw.println("pk,name,username,password,email,type,active,key");
+            }
+
+            String line = "\n" + currentPk++ + "," +
+                    user.getName() + "," +
+                    user.getUsername() + "," +
+                    user.getPassword() + "," +
+                    user.getEmail() + "," +
+                    "default" + "," +
+                    "true" + "," +
+                    generateRandomKey();
+
+            pw.print(line);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String generateRandomKey() {
+        return java.util.UUID.randomUUID().toString();
+    }
+}
--- shop/src/shop/Shop.java
+++ shop/src/shop/Shop.java
@@ -1,7 +1,14 @@
 package shop;
 
+// import CSV class
+import shop.CSVwriter.CSVWriter;
+
 public class Shop {
     public static void main(String[] args) {
-        System.out.println("Bienvenidos");
+        System.out.println("hola mundo");
+
+        // create user
+        User user1 = new User("Name", "UserName", "Password", "Email@example.com");
+        CSVWriter.appendUserToCSV(user1);
     }
 }

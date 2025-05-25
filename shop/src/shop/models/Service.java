b14ee3a1429f68c122962a9c242bd5163692e82c
titooDiaz <miguelitodiaz169@gmail.com>
2025-05-25 18:54:38 -0500

    add service 💁

--- shop/src/shop/models/Service.java
+++ shop/src/shop/models/Service.java
@@ -1,21 +1,26 @@
 package shop.models;
-import java.util.Date;
 
 public class Service {
-    // attributes
+    // Attributes
     private int pk;
-    private float purchase_price;
-    private PaidMethod paidMethod;
-    private Date date;
+    private String date;
     private float price;
+    private int paidMethod;
+    private int services;
 
 
     // builder
-    public Service(int pk, float purchase_price, PaidMethod paidMethod, Date date, float price){
+    public Service(int pk, int paidMethod, String date, float price){
         this.pk = pk;
-        this.purchase_price = purchase_price;
         this.paidMethod = paidMethod;
         this.date = date;
+        this.price = price;
+    }
+
+    public Service(String date, int paidMethod, int services, float price) {
+        this.paidMethod = paidMethod;
+        this.date = date;
+        this.services = services;
         this.price = price;
     }
     
@@ -26,26 +31,26 @@ public class Service {
     public void setPk(int pk) {
         this.pk = pk;
     }
-    public float getPurchase_price() {
-        return purchase_price;
-    }
-    public void setPurchase_price(float purchase_price) {
-        this.purchase_price = purchase_price;
-    }
-    public PaidMethod getPaidMethod() {
+    public int getPaidMethod() {
         return paidMethod;
     }
-    public void setPaidMethod(PaidMethod paidMethod) {
+    public void setPaidMethod(int paidMethod) {
         this.paidMethod = paidMethod;
     }
-    public Date getDate() {
+    public int getServices() {
+        return paidMethod;
+    }
+    public void setPaidServices(int paidMethod) {
+        this.paidMethod = paidMethod;
+    }
+    public String getDate() {
         return date;
     }
-    public void setDate(Date date) {
+    public void setDate(String date) {
         this.date = date;
     }
     public float getPrice() {
-        return price;
+        return this.price;
     }
     public void setPrice(float price) {
         this.price = price;
